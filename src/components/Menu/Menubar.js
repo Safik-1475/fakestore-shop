d39c2db98102773a7e7aa6c599af4f47260e820c
Safi <safikurrahman7514@gmail.com>
2022-03-19 20:08:15 +0600

    added column

--- src/components/Menu/Menubar.js
+++ src/components/Menu/Menubar.js
@@ -4,31 +4,28 @@ const Menubar = () => {
     // console.log(props)
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg  bg-light">
                 <div className="container ">
-                    <div className="d-flex justify-content-between">
-                        <a className="navbar-brand" href="olmo">OLMO</a>
+                    <a className="navbar-brand" href="olmo">OLMO</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav  mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link text-capitalize active" aria-current="page" href="#home">home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link text-capitalize" href="#cart">cart</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link text-capitalize" href="#contact">contact</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link text-capitalize" href="#login">login</a>
-                                </li>
-                            </ul>
-                        </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav  mb-2 mb-lg-0 d-flex justify-content-end">
+                            <li className="nav-item">
+                                <a className="nav-link text-capitalize active" aria-current="page" href="#home">home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-capitalize" href="#cart">cart</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-capitalize" href="#contact">contact</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-capitalize" href="#login">login</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const SingleProduct = (props) => {
-    const { id, rating, title, price, description, category, image } = props.singleProduct
+    const { rating, title, price, description, category, image } = props.singleProduct
+    console.log(props.rating)
     return (
         <div className="col-12 col-sm-12 col-md-6 col-lg-4 my-3">
             <div className="card">
-                <img src={image} alt="" className='img-responsive w-50 my-2 mx-auto' />
+                <img src={image} alt="" className='img-responsive w-50 py-5 mx-auto' />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
-                    <h5 className='text-danger'> $ {price}</h5>
-                    <h5 className='text-danger'>{ }</h5>
+                    <div className="d-flex justify-content-around">
+                        <h5 className='text-danger'> $ {price}</h5>
+
+                    </div>
                     <p className="card-text">{description.slice(0, 130)}</p>
                 </div>
             </div>
